7b90abc506f9bf8ab6b3a00296d4c6fe3dd20397
kevinelong <kevinelong@gmail.com>
2021-02-04 14:24:13 -0800

    font

---
+++ font.py
@@ -0,0 +1,328 @@
+segment_data = """
+
+ OOOO
+O    O
+O    O
+O    O
+ OOOO
+O    O
+O    O
+O    O OO
+ OOOO  OO
+
+"""
+
+number_data = """
+
+  OO
+ O O
+   O
+   O
+   O 
+ OOOOO
+
+ OOOO
+O    O
+     O
+   OO
+  O
+ O
+OOOOOO
+
+OOOOOO
+    O
+   O
+  OOO
+     O
+O    O
+ OOOO
+
+   OO
+  O O
+ O  O
+O   O
+OOOOOO
+    O
+    O
+    O
+
+OOOOOO
+O
+O
+OOOOO
+     O
+     O
+O    O
+ OOOO
+
+   O
+  O
+ O
+O
+OOOOO
+O    O
+O    O
+ OOOO
+
+ OOOOOO
+ O    O
+     O
+    O
+   O
+   O
+   O
+
+ OOOO
+O    O
+O    O
+ OOOO
+O    O
+O    O
+ OOOO
+
+ OOOO
+O    O
+O    O
+ OOOOO
+    O
+   O
+  O
+ O
+
+"""
+
+alpha_data = """
+
+  OO
+ O  O
+O    O
+O    O
+OOOOOO
+O    O
+O    O
+O    O
+
+OOOOO
+ O   O
+ O   O
+ OOOO
+ O   O
+ O   O
+ O   O
+OOOOO
+
+ OOOO
+O    O
+O    O
+O
+O
+O    O
+O    O
+ OOOO
+
+OOOOO
+ O   O
+ O    O
+ O    O
+ O    O
+ O    O
+ O   O
+OOOOO
+
+OOOOOOOO
+ O     O
+ O
+ OOOOO
+ O
+ O
+ O     O
+OOOOOOOO
+
+OOOOOOOO
+ O     O
+ O
+ OOOOO
+ O
+ O
+ O 
+OOO
+ 
+ OOOOO
+O     O
+O 
+O    OOO
+O     O
+O     O
+O     O
+ OOOOO
+
+OOO  OOO
+ O    O
+ O    O
+ OOOOOO
+ O    O
+ O    O
+ O    O
+OOO  OOO
+
+OOO
+ O
+ O
+ O
+ O
+ O
+ O
+OOO
+
+     OOO
+      O
+      O
+      O
+OOO   O
+ O    O
+  OOOO
+
+OOO  OOO
+ O    O
+ O   O
+ OOOO
+ O  O
+ O   O
+ O    O
+OOO  OOO
+
+  OO
+   O
+   O
+   O
+   O
+   O
+   O
+   OO
+
+OOO  OOO
+ O    O
+ OO  OO
+ O OO O
+ O    O
+ O    O
+ O    O
+OOO  OOO
+
+OOO  OOO
+ OO   O
+ O O  O
+ O  O O
+ O   OO
+ O    O
+ O    O
+OOO  OOO
+
+  OOOO
+ O    O
+O      O
+O      O
+O      O
+O      O
+ O    O
+  OOOO
+
+OOOOOO
+ O    O
+ O    O
+ OOOOO
+ O
+ O
+ O
+OOO
+
+  OOOO
+ O    O
+O      O
+O      O
+O      O
+O    O O
+ O    O
+  OOOO O
+
+OOOOOOO
+ O     O
+ O     O
+ OOOOOO
+ O     O
+ O     O
+ O     O
+OOO   OOO
+
+ OOOOOO
+O      O
+O
+ O
+  OOOO
+      O
+       O
+O      O
+ OOOOOO
+
+OOOOOOO
+O  O  O
+   O
+   O
+   O
+   O
+   O
+  OOO
+
+O      O
+O      O
+O      O
+O      O
+O      O
+O      O
+O      O
+ OOOOOO
+
+O      O
+O      O
+ O    O
+ O    O
+  O  O
+  O  O
+   OO
+
+O      O
+O      O
+O  OO  O
+O  OO  O
+O O  O O
+O O  O O
+OO    OO
+OO    OO
+
+OO    OO
+ O    O
+  O  O
+   OO
+   OO
+  O  O
+ O    O
+OO    OO
+
+OO   OO
+ O   O
+  O O
+  O O
+   O
+   O
+   O
+  OOO
+
+OOOOOOOO
+      O
+     O
+    O
+   O
+  O
+ O
+OOOOOOOO
+"""
